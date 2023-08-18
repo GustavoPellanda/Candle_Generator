066e214ff6c687f7591d801c3bf732d6ba8c7702
Gustavo Pellanda <gusfipe@gmail.com>
2023-08-17 23:44:39 -0300

    Initial setup configured, receives an answer from the API

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import axios from 'axios';
+
+config();
+
+// Fetches the API answer for the path defined in the .env file
+const readMarketPrice = async (): Promise<number> => {
+    const result = await axios.get(process.env.PRICES_API);
+    const data = result.data;
+    const price = data.bitcoin.usd;
+    console.log("Curent Bitcoin value is: USD", price);
+    return price;
+}
+
+readMarketPrice();
