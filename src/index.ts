e99a17690ad12587ddce4d6b4780509d52f0faf0
Gustavo Pellanda <gusfipe@gmail.com>
2023-08-23 00:29:29 -0300

    The API fetch is used to build candle objects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { config } from 'dotenv';
 import axios from 'axios';
+import Period from './enums/Period';
+import Candle from './models/Candle';
 
 config();
 
-// Fetches the API answer for the path defined in the .env file
+// Fetches and returns the API answer for the path defined in the .env file
 const readMarketPrice = async (): Promise<number> => {
     const result = await axios.get(process.env.PRICES_API);
     const data = result.data;
@@ -13,3 +15,27 @@ const readMarketPrice = async (): Promise<number> => {
 }
 
 readMarketPrice();
+
+// Creates a candle periodically:
+const generateCandles = async() => {
+    while(true){
+        const loopTimes = Period.FIVE_MINUTES  / Period.TEN_SECONDS;    // Sets the duration time of a candle
+        const candle = new Candle('BTC', new Date());                   // Constructs a candle
+
+        console.log("Generating a new candle...");
+
+        for(let i = 0; i < loopTimes; i++){
+            const price = await readMarketPrice();                      // Gets the API values
+            candle.addValue(price);                                     // Sends the values to the method that manages them
+            await new Promise(r => setTimeout(r, Period.TEN_SECONDS));  // Delays the time between each API fetch
+        }
+    
+        candle.closeCandle();
+        console.log("Candle finished.");
+        
+        const candleObj = candle.toSimpleObject();
+        console.log(candleObj);                                         // Logs the resulting candle object created
+    }
+}
+
+generateCandles();
